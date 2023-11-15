e0728935297e2523f8f7ef695ec9d51aa407c471
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-11-15 16:24:15 +0200

    $event, [value]

--- 02components/components-demo/src/app/home-page/home-page.component.ts
+++ 02components/components-demo/src/app/home-page/home-page.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
+import { User } from 'src/types/User';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
+
 export class HomePageComponent {
+  users: User[]
   toggle = false;
-  users = [{ name: 'ilich', age: 26 }, { name: 'sonya', age: 22 }, { name: "megi", age: 23 }];
-  onToggle = () => {
+  counter = 1;
+
+  constructor(){
+    this.users = [{ name: 'ilich', age: 26 }, { name: 'sonya', age: 22 }, { name: "megi", age: 23 }];
+  }
+  onToggle = (event: Event) => {
     this.toggle = !this.toggle;
+    this.counter++;
   }
 }
