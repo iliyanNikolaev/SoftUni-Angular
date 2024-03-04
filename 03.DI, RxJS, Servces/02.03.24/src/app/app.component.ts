9e4b0d76b2a33b5bb0b21330f420f4a3c5fe7aaa
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-03-04 09:23:21 +0200

    angular services

--- 03.DI, RxJS, Servces/02.03.24/src/app/app.component.ts
+++ 03.DI, RxJS, Servces/02.03.24/src/app/app.component.ts
@@ -1,41 +1,12 @@
 import { Component } from '@angular/core';
 import { Arduino } from './types/Arduino';
+import { ArduinoService } from './arduino.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'playground';
-  arduinos: Arduino[] = [{
-    model: 'Uno',
-    microcontroller: 'ATmega328P 16 MHz',
-    image: 'https://docs.arduino.cc/static/bbceab04f8e0726194ef4dfe2457097f/image.svg'
-  },
-  {
-    model: 'Leonardo',
-    microcontroller: 'ATmega32U4 16 MHz',
-    image: 'https://docs.arduino.cc/static/4c88ffc380ff7cbc0e7340c00870501f/image.svg'
-  },
-  {
-    model: 'Nano',
-    microcontroller: 'ATmega328',
-    image: 'https://docs.arduino.cc/static/3f2881bc823724bfa1f0b586a830bde1/image.svg'
-  }
-  ]
-
-  handleCreate(model: HTMLInputElement, microcontroller: HTMLInputElement, image: HTMLInputElement) {
-    if (!model.value || !microcontroller.value || !image.value) {
-      throw new Error('all fields are required');
-    }
-    this.arduinos.push({
-      model: model.value,
-      microcontroller: microcontroller.value,
-      image: image.value
-    });
-    model.value = '';
-    microcontroller.value = '';
-    image.value = '';
-  }
+export class AppComponent { 
+  constructor(public sArduino: ArduinoService) { }
 }
