d9f4d27efc12099fd15822b67fc2a548cc9d0263
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-03-04 14:01:31 +0200

    RxJS observers

--- 03.DI, RxJS, Servces/02.03.24/src/main.ts
+++ 03.DI, RxJS, Servces/02.03.24/src/main.ts
@@ -1,6 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { Observable } from 'rxjs';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
@@ -16,3 +17,26 @@ platformBrowserDynamic().bootstrapModule(AppModule)
 // }
 
 // console.log('monkey patching demo');
+
+
+// OBSERVERS
+
+const interval = (ms: number) => {
+  const o = new Observable<string>((observer) => {
+    let counter = 0;
+    const timer = setInterval(() => {
+      observer.next(`from observer: ${counter}`);
+      counter++;
+    }, ms);
+
+    return () => {
+      clearInterval(timer);
+    }
+  });
+
+  return o;
+}
+
+interval(2000).subscribe(data => {
+  console.log(data);
+})
