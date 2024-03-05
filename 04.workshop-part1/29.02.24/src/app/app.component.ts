5104ece0adbb0cdfb716e58dd4262d04314e9d48
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-03-05 19:21:39 +0200

    prepare resources for workshop

--- 04.workshop-part1/29.02.24/src/app/app.component.ts
+++ 04.workshop-part1/29.02.24/src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = '29.02.24';
-}
+export class AppComponent { }
--- 04.workshop-part1/29.02.24/src/app/app.module.ts
+++ 04.workshop-part1/29.02.24/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
