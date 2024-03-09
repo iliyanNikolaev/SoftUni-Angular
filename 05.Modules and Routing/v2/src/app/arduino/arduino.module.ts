781bc95235669e53f0315c10f1167c8208d8c742
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-03-09 21:09:03 +0200

    preparing resources for modules and routing demo

---
+++ 05.Modules and Routing/v2/src/app/arduino/arduino.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListComponent } from './list/list.component';
+import { ItemComponent } from './item/item.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListComponent,
+    ItemComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ArduinoModule { }
