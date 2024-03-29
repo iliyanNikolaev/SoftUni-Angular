176becdfba71fff40c8e0546ea45c41e374c6597
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-03-29 16:44:11 +0200

    custom directive demo

--- 06.Forms/v2/src/app/shared/shared.module.ts
+++ 06.Forms/v2/src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
+import { HighlightDirective } from './highlight.directive';
 
 
 
 @NgModule({
   declarations: [
-    LoaderComponent
+    LoaderComponent,
+    HighlightDirective
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    LoaderComponent
+    LoaderComponent,
+    HighlightDirective
   ]
 })
 export class SharedModule { }
