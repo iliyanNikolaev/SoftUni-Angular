f94a6de537bd52824494279f9fb7cddcfa7bedf9
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-03-31 22:11:48 +0300

    date and async pipes

--- 07.pipes-interceptoprs-subjects/demo/src/app/app.component.ts
+++ 07.pipes-interceptoprs-subjects/demo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   data = { message: "hello" }
+  mockPromise = new Promise((res) => {
+    setTimeout(() => {
+      res({data: 'this is the data from mocked promise!'});
+    }, 3000);
+  });
+
+  clock$ = interval(1000).pipe(map((x) => new Date()));
 }
